bcae10afa32ea9ce0380e2789690ac6230f434ec
SudoSzune <robin1415@kookmin.ac.kr>
2024-10-28 19:16:39 +0900

    feat(Print): 각 시행횟수별 전진 결과 출력

--- src/App.js
+++ src/App.js
@@ -10,17 +10,32 @@ class App {
     for(let i = 0; i < arr.length; i++){
       arr[i] = 0; //0으로 초기화
     }
+    try{
+      for(let i=0; i<ATTEMPT; i++){
+        this.race(arr);
+        this.print(arr, CARS);
+      }
+    }catch(error){
+      console.error(error.message);
+    }
   }
-  race(attempt, arr){
-    for(let i=0; i<attempt; i++){
-      for(let j=0; j<arr.length; j++){
-        let randomNumber = Random.pickNumberInRange(0,9);
-        if(randomNumber >= 4){
+  race(arr){
+    for(let j=0; j<arr.length; j++){
+      let randomNumber = Random.pickNumberInRange(0,9);
+      if(randomNumber >= 4){
           arr[j]++;
-        }
       }
     }
   }
+
+  print(arr, cars) {
+    for (let i = 0; i < arr.length; i++) {
+      let result = `${cars[i]} : `;
+      result += '-'.repeat(arr[i]); // 전진 횟수만큼 '-'를 추가
+      console.log(result); // 한 줄로 출력
+    }
+  }
+
 
 
 }
